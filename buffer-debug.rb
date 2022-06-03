2142f888ccd54fd3db603aafe3649d6754a6d97d
victor cordeiro <victor.cdp97@gmail.com>
2022-06-02 21:39:10 -0300

    buffer and debug version to see whats happening

---
+++ buffer-debug.rb
@@ -0,0 +1,26 @@
+buffer = []
+
+producer = Thread.start {
+    while (true)
+        if (buffer.length <= 5)
+            newnumber = rand(100)
+            puts "put in number: #{newnumber}, buffer is now #{buffer << newnumber}"
+            sleep(rand(0.5))
+        else
+            sleep(rand(5))
+        end
+    end
+}
+ 
+consumer = Thread.start {
+    while (true)
+        if (!buffer.empty?)
+            puts "took out number: #{buffer.shift}"
+            sleep(rand(0.5))
+        else
+            sleep(rand(5))
+        end
+    end
+}
+
+sleep(100)
---
+++ buffer.rb
@@ -0,0 +1,25 @@
+buffer = []
+
+producer = Thread.start {
+    while (true)
+        if (buffer.length <= 5)
+            buffer << rand(100)
+            sleep(rand(0.5))
+        else
+            sleep(rand(5))
+        end
+    end
+}
+ 
+consumer = Thread.start {
+    while (true)
+        if (!buffer.empty?)
+            puts "took out number: #{buffer.shift}"
+            sleep(rand(0.5))
+        else
+            sleep(rand(5))
+        end
+    end
+}
+
+sleep(100)
